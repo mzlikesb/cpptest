29d4b0f672eb6cd894cfa96838f8912afa18e7f0
dy <mintzzzzz42@gmail.com>
2019-08-21 12:37:37 +0900

    짝수합 홀수합 최적화

--- 0820/main.cpp
+++ 0820/main.cpp
@@ -1,6 +1,14 @@
 #include <stdio.h>
+#include "utils.h"
 
 int main() {
-	// 안녕
+
+	int num;
+	printf("자연수 : ");
+	scanf("%d", &num); // 입력받을때 &써야함	
+	
+	printf("%d\n", sum(num));
+	printf("%d", CalculateOddSum(num));
+
 	return 0;
 }
